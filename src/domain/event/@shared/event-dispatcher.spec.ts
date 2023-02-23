523d2c2a6f48f4d492a9e540ca3975871238ccbd
Luciano <luciano.horacio@evo.com.br>
2023-02-23 06:18:33 -0300

    event

---
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -0,0 +1,18 @@
+
+describe("Domain event tests", () => {
+    it("Should register an event handler", () => {
+
+        /*
+
+        const eventDispatcher = new EventDispatcher();
+        const evetnHandler = new sendEmailWhenProductIsCreatedHandler();
+
+        eventDispatcher.register("ProductCreatedEvent", evetnHandler);
+        
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1);
+        */
+
+    });
+});
+
