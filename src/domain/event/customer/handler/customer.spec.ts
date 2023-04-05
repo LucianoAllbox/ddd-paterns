650ecdbdc35251dbfe316971c92efebbf9fcd7d3
Luciano <luciano.horacio@evo.com.br>
2023-04-05 10:14:55 -0300

    ajuste de pastas

--- src/domain/event/customer/handler/customer.spec.ts
+++ src/domain/event/customer/handler/customer.spec.ts
@@ -1,5 +1,5 @@
-import Address from "../value-object/address";
-import Customer from "./customer";
+import Address from "../../../customer/value-object/address";
+import Customer from "../../../customer/entity/customer";
 
 describe("Customer unit tests", () => {
 
